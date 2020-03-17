0545eb992f40936b84f2aa16cfe4f5fc251460ac
ea-guerette <ea-guerette@users.noreply.github.com>
2020-03-17 14:27:28 +1100

    Commented code and added data so repos has everything (reproducible)

--- bom_analysis.R
+++ bom_analysis.R
@@ -1,5 +1,11 @@
+#Use the data files provided to answer the Qs in "Pulling it all together" 
+#part of the Reproducibility challenge (week2)
+
+#use the tools we have learnt in the course so far: 
 library(tidyverse)
 
+#read in provided data files 
+#tidy them up 
 
 bom_data <- read_csv("data/BOM_data.csv")
 #Temp_min_max need separating 
@@ -9,6 +15,9 @@ bom_meta <- read_csv("data/Bom_stations.csv")
 #this is wide, would be better as a long data set 
 bom_meta_long <- gather(bom_meta, key = "Station_number", value = "amount", -info)
 meta <- spread(bom_meta_long, key = "info", value = "amount")
+
+#bom_data_sep and meta are the 'tidy' versions 
+
 
 
 #Q1 
@@ -26,7 +35,8 @@ ans_Q1 <-filter(bom_data_sep, min_temp != "-", max_temp != "-", Rainfall != "-" 
 #group by month 
 #calculate diff between max_temp and min_temp and save that in new column
 #calc monthly mean 
-#
+#filter to only show row that contains the lowest mean daily temperature difference
+
 ans_Q2 <- group_by(bom_data_sep, Month) %>% 
   mutate(daily_temp_diff = as.numeric(max_temp) - as.numeric(min_temp)) %>% 
   summarise(mean_daily_temp_diff = mean(daily_temp_diff, na.rm = TRUE)) %>% 
@@ -35,15 +45,20 @@ ans_Q2 <- group_by(bom_data_sep, Month) %>%
 #Q3 
 #Which state saw the lowest average daily temperature difference?
 
-#need to combine/join data sets to answer this Q
+#need to combine/join data sets to answer next two Qs so saving as new tibble:
 bom_data_sep <- mutate(bom_data_sep, Station_number = as.character(Station_number))
 joined_data <- full_join(bom_data_sep, meta) 
+
+#group by state 
+#calculate temp differences 
+#filter to show row that contains lowest mean daily temperature difference
 
 ans_Q3 <- group_by(joined_data, state) %>% 
   mutate(daily_temp_diff = as.numeric(max_temp) - as.numeric(min_temp)) %>% 
   summarise(mean_daily_temp_diff = mean(daily_temp_diff, na.rm = T)) %>% #can also use drop.na() instead
   filter(mean_daily_temp_diff == min(mean_daily_temp_diff))
-# a lot of this code is reused from Q3 - this could be streamlined
+# two lines of this code is reused from Q3 - this could be streamlined
+# save bom_data_w_temp_diff and use this instead of starting from bom_data_sep in both Q2 and Q3
 
 
 #Q4
@@ -51,12 +66,13 @@ ans_Q3 <- group_by(joined_data, state) %>%
 #weather station in our dataset have a higher average solar exposure?
 
 #using joined_data again 
-
+#group by longitude
+#calculate mean solar exposure 
+#arrange #BUT IDEALLY I WOULD LIKE TO EXTRACT ONLY THE ANSWER
 tib_Q4 <- group_by(joined_data, lon) %>% 
-  summarise(mean_solar_exp = mean(as.numeric(Solar_exposure), na.rm = T))  
+  summarise(mean_solar_exp = mean(as.numeric(Solar_exposure), na.rm = T))  %>% 
+  arrange(lon)
 
-west <- filter(tib_Q4,lon == min(as.numeric(lon))) 
-east <-  filter(tib_Q4, lon == max(as.numeric(lon))) 
-ansQ4 <- west < east
+ansQ4 <-filter(tib_Q4,lon == min(as.numeric(lon)))  <  filter(tib_Q4, lon == max(as.numeric(lon))) 
 #messy - but the westmost station gets less solar exposure
 
