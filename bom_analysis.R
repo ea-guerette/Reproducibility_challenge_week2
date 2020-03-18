0c7e49854dcd8fa95f55fc56369539a911d50319
Stephen <36123293+sparce@users.noreply.github.com>
2020-03-18 15:12:18 +1100

    Option to cut down on a little bit of repetition in Q4

--- bom_analysis.R
+++ bom_analysis.R
@@ -87,7 +87,7 @@ group_by(joined_data, lon, Station_number) %>%
 #ANOTHER, BETTER WAY
 tib_Q4 <- group_by(joined_data, lon) %>% 
   summarise(mean_solar_exp = mean(as.numeric(Solar_exposure), na.rm = T))  %>% 
-  filter(lon == max(as.numeric(lon)) | lon == min(as.numeric(lon)))
+  filter(lon %in% range(as.numeric(lon)))
 
 ansQ4 <-filter(tib_Q4, mean_solar_exp == max(mean_solar_exp))
 
